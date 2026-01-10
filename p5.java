0654b32be3ec65f58948000bc6bfbb513f7f084f
divya <divyaparth2003@gmail.com>
2026-01-10 21:49:28 +0530

    Update p5.java

--- p5.java
+++ p5.java
@@ -4,28 +4,25 @@ import java.util.Scanner;
  public static void main(String[] args) {
 
     Scanner sc = new Scanner(System.in);
+  
+     System.out.println("Enter the value of amount");
+     int amount = sc.nextInt();
+     System.out.println("the amount is : "  +amount);
 
+     int notes[] = {100,50,10,5,2,1};
+     System.out.println("notes : ");
 
-System.out.println("Enter the value of amount");
- int amount = sc.nextInt();
-
- System.out.println("the amount is : "  +amount);
-
-int notes[] = {100,50,10,5,2,1};
-
-System.out.println("notes : ");
-
-for(int i = 0 ;i<notes.length; i++)
-  {
-     int count = amount / notes[i];
-     if(count > 0 ){
-         //System.out.println( notes[i]":"  +count );
-         System.out.println(notes[i] + " : " + count);
-         amount = amount % notes[i];
-       }
+     for(int i = 0 ;i<notes.length; i++)
+      {
+          int count = amount / notes[i];
+          if(count > 0 ){
+          System.out.println(notes[i] + " : " + count);
+           amount = amount % notes[i];
+      }
    }
 
    sc.close();
 
-}
+   }
+
 }
