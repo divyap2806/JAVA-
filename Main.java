58f5af501c0fbeecdf26ced6b4a4c20afb292517
divya <divyaparth2003@gmail.com>
2026-01-29 19:39:21 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static String intToRoman(int num) {
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+        StringBuilder result = new StringBuilder();
+
+        for(int i = 0; i < values.length; i++) {
+            while(num >= values[i]) {
+                result.append(symbols[i]);
+                num -= values[i];
+            }
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter an integer to convert to Roman numeral: ");
+        int num = sc.nextInt();
+
+        String roman = intToRoman(num);
+        System.out.println(num + " -> " + roman);
+
+        sc.close();
+    }
+}
