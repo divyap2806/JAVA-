d73530a2465e08126ce76347af968cf0483e891f
divya <divyaparth2003@gmail.com>
2026-02-10 22:31:27 +0530

    Add files via upload

---
+++ Escape_seque.java
@@ -0,0 +1,18 @@
+public class Escape_seque{
+
+public static void main(String[] args){
+
+System.out.println("divya\nraval");
+
+System.out.println("divya\traval");
+
+System.out.println("divya\\raval\\");
+
+System.out.println("\"divya\"\\raval\\\"");
+
+System.out.println("divya\' raval");
+
+
+}
+
+}
